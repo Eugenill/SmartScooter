0d4c473c0945d0a0dc99feb2bff95caf8a8a5b7c
Martin Aalby Svalesen <martiasv@stud.ntnu.no>
2020-04-13 08:56:43 +0200

    Now with MQTT on multiple threads

---
+++ TRAFFIC_LIGHTS/Prediction/Single_threaded/pred.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2 as cv
+
+cvNet = cv.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
+
+img = cv.imread('test.jpg')
+rows = img.shape[0]
+cols = img.shape[1]
+cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
+cvOut = cvNet.forward()
+
+for detection in cvOut[0,0,:,:]:
+    score = float(detection[2])
+    if score > 0.3:
+        left = detection[3] * cols
+        top = detection[4] * rows
+        right = detection[5] * cols
+        bottom = detection[6] * rows
+        cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+
+cv.imshow('img', img)
+cv.waitKey()
