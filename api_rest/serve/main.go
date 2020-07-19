ed5ffbe1fde40398094e9430c9c3a697767b8f6e
Eugeni <eugeni.llagostera@gmail.com>
2020-07-19 13:07:15 +0200

    New organization

---
+++ api_rest/serve/main.go
@@ -0,0 +1,9 @@
+package main
+import (
+	"github.com/go-chi"
+
+)
+
+func main() {
+
+}
---
+++ api_rest/sqlbunny/main.go
@@ -0,0 +1,10 @@
+package sqlbunny
+
+import (
+	"github.com/sqlbunny"
+	"github.com/sqlbunny/sqlbunny/gen/core"
+	"github.com/sqlbunny/sqlbunny/gen/migration"
+	"github.com/sqlbunny/sqlbunny/gen/stdtypes"
+
+
+)
