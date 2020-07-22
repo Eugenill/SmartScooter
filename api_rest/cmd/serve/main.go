aa0b960908088634ca20c2218c50c6a9e9b86518
Eugeni <eugeni.llagostera@gmail.com>
2020-07-22 14:10:36 +0200

    Redistribution

--- api_rest/cmd/serve/main.go
+++ api_rest/cmd/serve/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"github.com/Eugenill/SmartScooter/api_rest/errors"
 	"github.com/Eugenill/SmartScooter/api_rest/mqtt"
+	"github.com/Eugenill/SmartScooter/api_rest/pkg/errors"
 	"github.com/Eugenill/SmartScooter/api_rest/router"
 	"log"
 	"net/http"
 	"net/url"
 )
+
+var mqttConfig = mqtt.MQTTConfig{
+	Host:     mqtt.MQTTHost,
+	Port:     mqtt.MQTTPort,
+	User:     url.UserPassword(mqtt.MQTTUsername, mqtt.MQTTPassw),
+	Pretopic: mqtt.MQTTPreTopic,
+	ClientID: mqtt.MQTTCLientID,
+}
 
 func main() {
 	//1. Create Context
@@ -15,20 +23,14 @@ func main() {
 	//2. Open DB
 
 	//3. Initialize MQTT
-	mqttConfig := mqtt.MQTTConfig{
-		Host:     mqtt.MQTTHost,
-		Port:     mqtt.MQTTPort,
-		User:     url.UserPassword(mqtt.MQTTUsername, mqtt.MQTTPassw),
-		Pretopic: mqtt.MQTTPreTopic,
-		ClientID: mqtt.MQTTCLientID,
-	}
 	client := mqtt.ConnectToBroker(mqttConfig.ClientID, mqttConfig)
 	log.Printf("Connected to the MQTTbroker, Host:%s, Port%s", mqttConfig.Host, mqttConfig.Port)
 	topics := []string{"timer", "detection"}
 	for _, topic := range topics {
 		go mqtt.ListenToTopic(mqttConfig, topic, client)
-		//mqtt_client.PublishTimer("timer", mqttConfig)
 	}
+	//mqtt_client.PublishTimer("timer", mqttConfig)
+
 	//4. Init router
 	err := http.ListenAndServe("localhost:1234", router.SetRouter(mqttConfig))
 	errors.Catch(err)
