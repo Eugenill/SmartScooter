fbc5c0d65d7255665dbdd28df05babc3757e2998
Eugeni <eugeni.llagostera@gmail.com>
2020-09-04 14:43:10 +0200

    dbSource, fmtSprintf

--- api_rest/pkg/db/db.go
+++ api_rest/pkg/db/db.go
@@ -3,16 +3,26 @@ package db
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/Eugenill/SmartScooter/api_rest/pkg/errors"
 	"github.com/gin-gonic/gin"
 	"github.com/sqlbunny/sqlbunny/runtime/bunny"
 	"log"
 )
 
+const (
+	dbName = "smartscooter"
+	dbUser = "postgres"
+	dbPass = "postgres"
+	dbHost = "localhost"
+	dbPort = "5432"
+)
+
 var DB *sql.DB
 
 func OpenDB() *sql.DB {
-	db, err := sql.Open("postgres", "host=localhost port=5432 dbname=smartscooter user=postgres password=postgres sslmode=disable")
+	dbSource := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", dbHost, dbPort, dbName, dbUser, dbPass) //%s for strings, %d for int
+	db, err := sql.Open("postgres", dbSource)
 	errors.PanicError(err)
 	log.Printf("Connected to the PostgreSQL, Host: localhost, Port: 5432, dbname: smartscooter, user: postgres, password: postgres")
 	return db
